e6ef96923aa752243a4753b3c9802c9c95925a4f
Dhruv Chawla <dchawla228@gmail.com>
2022-08-01 16:13:27 +0530

    function to fetch the access token

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ import os
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-}
-
 CLIENT_ID = os.environ['CONSUMER_KEY']
 CLIENT_SECRET = os.environ['CONSUMER_SECRET']
 PASSWORD = os.environ['PASSWORD']
 
-data = f'grant_type=password&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&username=dhruv@growth-spree.com&password={PASSWORD}'
 
-response = requests.post('https://growthspree-dev-ed.my.salesforce.com/services/oauth2/token', headers=headers, data=data)
-print(response.text)
+def get_access_token():
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    data = f'grant_type=password&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&username=dhruv@growth-spree.com&password={PASSWORD}'
+    response = requests.post('https://growthspree-dev-ed.my.salesforce.com/services/oauth2/token', headers=headers, data=data)
+    
+    return eval(response.text)['access_token']
