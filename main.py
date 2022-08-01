65b92e39278d0095cd9a2fe959d80a5b9b22992e
Dhruv Chawla <dchawla228@gmail.com>
2022-08-01 17:17:13 +0530

    get user data using user_id

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import json
 
 # Load .env file
 env_path = Path('.') / '.env'
@@ -10,13 +11,24 @@ load_dotenv(dotenv_path=env_path)
 CLIENT_ID = os.environ['CONSUMER_KEY']
 CLIENT_SECRET = os.environ['CONSUMER_SECRET']
 PASSWORD = os.environ['PASSWORD']
+DOMAIN_URL = 'https://growthspree-dev-ed.my.salesforce.com'
 
-
+# Get access token
 def get_access_token():
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
     }
     data = f'grant_type=password&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&username=dhruv@growth-spree.com&password={PASSWORD}'
-    response = requests.post('https://growthspree-dev-ed.my.salesforce.com/services/oauth2/token', headers=headers, data=data)
+    response = requests.post(f'{DOMAIN_URL}/services/oauth2/token', headers=headers, data=data)
     
     return eval(response.text)['access_token']
+
+# Get user data using user_id
+def get_user_data(user_id):
+    access_token = get_access_token()
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(f'{DOMAIN_URL}/services/data/v55.0/sobjects/Contact/{user_id}', headers=headers)
+    return json.dumps(json.loads(response.text), indent = 1)
+    
